b317c0d711b68bcae0cc673ccba538d034904cf2
Neo <wupeng@touker.com>
2017-06-26 17:13:38 +0800

    ”State Updates are Merged“

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const a = (
+React.createElement
+
+class App extends Component {
+  render() {
+    return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -11,12 +15,49 @@ const a = (
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Welcome name={this.props.name} />
+        <MyDate/>
       </div>
     );
-class App extends Component {
+  };
+}
+
+class Welcome extends Component {
   render() {
-    return a;
+    return (
+      <div>
+          <p>Hello {this.props.name}</p>
+      </div>
+    );
   }
 }
 
-export default App;
+class MyDate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date:new Date()}
+  }
+  componentDidMount () {
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 100);
+  }
+  componentWillUnmount () {
+    clearInterval(this.timerID);
+  }
+  tick(){
+    this.setState((prevState,props)=>(
+      {
+        date:new Date()
+      }
+    ));
+  }
+  render () {
+    return (
+      <p>Current Date is: {this.state.date.toLocaleTimeString()}</p>
+    )
+  }
+}
+
+export {Welcome,App as default,MyDate};
+// export default App;
